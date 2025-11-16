b2325bdcaaed750b146b8c0f1c47e707d9b434c0
Pedro <pedroprocuroemprego@gmail.com>
2025-11-16 16:56:39 -0300

    Api adicionada ao projeto

--- src/Api.java
+++ src/Api.java
@@ -1,6 +1,21 @@
+import java.util.Map;
+
+/**
+ * Classe que define a estrutura de dados (Record) para a resposta da ExchangeRate-API.
+ */
 public class Api {
 
+    // Record para facilitar a manipulação dos dados JSON
+    public record ExchangeRateApi(String result, String base_code, Map<String, Double> conversion_rates) {
 
+        @Override
+        public String toString() {
+            return  "Código da Moeda Base: " + base_code +
+                    "\n Conversões de Valores (selecionadas): " + conversion_rates;
+        }
 
-
+        public Map<String, Double> conversionRates() {
+            return conversion_rates;
+        }
+    }
 }
